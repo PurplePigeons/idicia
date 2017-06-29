04193793df9945a14bd7f77d933d6949e0289f83
Shawn Stern <stern.shawn@gmail.com>
2017-06-28 23:43:52 -0700

    Adding some documentation for blog api functions

--- client/app/utils/blogApi.js
+++ client/app/utils/blogApi.js
@@ -3,8 +3,22 @@ import { ajax } from 'rxjs/observable/dom/ajax';
 // Best practice to use rxjs/ajax functions since they return an Observable which is more easily
 // consumed by epics
 
+/**
+ * Fetch a page of posts determined by the given page number
+ *
+ * @param {int} page   A valid page number
+ *
+ * @return {object|null} Returns either the object containing the collection of posts, or throws an error
+ */
 export const fetchPageOfPosts = (page) =>
   ajax.getJSON(`/api/post/paginated/${page}`);
 
+/**
+ * Fetch the data for a single post object by its unique slug value
+ *
+ * @param {string} slug   A valid post slug
+ *
+ * @return {object|null} Returns either the object containing the post data, or throws an error
+ */
 export const fetchPostBySlug = (slug) =>
   ajax.getJSON(`/api/post/slug/${slug}`);
