c223c353e5d961744276dfb6b856ab3703c0d003
Shawn Stern <stern.shawn@gmail.com>
2017-07-04 00:21:08 -0700

    Adding auto-capitalization to sitePagesHandler
    -Just so there is less headache in case API user tries solutions instead of Solutions, etc
    -Don't necessarily want production page routes to be capitalized, makes sense to just do a tiny bit of extra processing when getting page data in the backend

--- routes/api/sitePages.js
+++ routes/api/sitePages.js
@@ -6,7 +6,10 @@ const keystone = require('keystone');
  * the Solutions schema
  */
 exports.sitePagesHandler = (req, res) => {
-  const schema = keystone.list(req.params.page);
+  // Convert incoming page param to uppercase to match Mongoose schema naming
+  const page = req.params.page[0].toUpperCase() + req.params.page.slice(1);
+  const schema = keystone.list(page);
+
   schema.model
     .find()
     .then(data => res.json({ page: data[0] }))
