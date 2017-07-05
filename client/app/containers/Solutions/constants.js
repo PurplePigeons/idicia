360d1519e95e917479a72aa859c76be15c016c4a
Shawn Stern <stern.shawn@gmail.com>
2017-07-04 22:49:39 -0700

    Fixing constant naming

--- client/app/containers/Solutions/constants.js
+++ client/app/containers/Solutions/constants.js
@@ -2,6 +2,6 @@
  * Action Constants
  */
 
-export const GET_PAGE = 'idicia/solutions/GET_STATIC_PAGE';
+export const GET_STATIC_PAGE = 'idicia/solutions/GET_STATIC_PAGE';
 export const GET_PAGE_FAILED = 'idicia/solutions/GET_PAGE_FAILED';
 export const SET_STATIC_PAGE_DATA = 'idicia/solutions/SET_STATIC_PAGE_DATA';
