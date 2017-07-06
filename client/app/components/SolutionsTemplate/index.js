1b1fb1b144daabea5dddadcf287d3df19d080256
Shawn Stern <stern.shawn@gmail.com>
2017-07-05 23:40:05 -0700

    Updating Solutions model and page rendering
    -Breaking up the contents of each type of solution (Verification, Healthcare, etc on the page) so that they can be rendered in their own divs and more easily styled in split column
    -Using Object.keys/filter/sort/map to render out each block like a functional boss
    -Adding custom scss to handle rendering into a split, wrapped column on tablets+ and normally on mobile #responsiveDesign
    -Updating Bulma SCSS vars to match Idicia link coloring
    -Factoring out common classnames into consts to clean up SolutionsTemplate code

--- client/app/components/SolutionsTemplate/index.js
+++ client/app/components/SolutionsTemplate/index.js
@@ -4,66 +4,83 @@ import PropTypes from 'prop-types';
 import bulma from 'styles/bulma.scss';
 import styles from './styles.scss';
 
-const SolutionsTemplate = ({ data }) => (
-  <div>
-    <section className={`${bulma.hero} ${bulma['is-small']} ${bulma['is-bold']} ${bulma['is-primary']}`}>
-      <div className={bulma['hero-body']}>
-        <div className={bulma.container}>
-          <h1 className={`${bulma.title} ${styles.mainTitle}`}>
-            {data.title}
-          </h1>
+const SolutionsTemplate = ({ data }) => {
+  // Generate the divs for the 6 distinct solutions
+  const solutionExamples = data.block3.column1 && Object.keys(data.block3.column1)
+    .filter((key) => key !== 'title')
+    .sort() // Since the keys come out in an indeterminate order... #object-things
+    .map((content) => <div key={content} dangerouslySetInnerHTML={{ __html: data.block3.column1[content].html }} />);
+
+  const boldHero = `${bulma.hero} ${bulma['is-bold']}`;
+  const mainHero = `${boldHero} ${bulma['is-small']} ${bulma['is-primary']}`;
+  const infoHero = `${boldHero} ${bulma['is-medium']} ${bulma['is-info']}`;
+  const lightHero = `${boldHero} ${bulma['is-medium']} ${bulma['is-light']}`;
+
+  const mediumCustomContent = `${bulma.content} ${bulma['is-medium']} ${styles.content}`;
+
+  return (
+    <div>
+      <section className={mainHero}>
+        <div className={bulma['hero-body']}>
+          <div className={bulma.container}>
+            <h1 className={`${bulma.title} ${styles.mainTitle}`}>
+              {data.title}
+            </h1>
+          </div>
         </div>
-      </div>
-    </section>
-    <section className={`${bulma.hero} ${bulma['is-medium']} ${bulma['is-bold']} ${bulma['is-info']}`}>
-      <div className={bulma['hero-body']}>
-        <div className={bulma.container}>
-          <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block1.html }} />
+      </section>
+      <section className={infoHero}>
+        <div className={bulma['hero-body']}>
+          <div className={bulma.container}>
+            <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block1.html }} />
+          </div>
         </div>
-      </div>
-    </section>
-    <section className={`${bulma.hero} ${bulma['is-medium']} ${bulma['is-bold']} ${bulma['is-light']}`}>
-      <div className={bulma['hero-body']}>
-        <div className={bulma.container}>
-          <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block2.html }} />
+      </section>
+      <section className={lightHero}>
+        <div className={bulma['hero-body']}>
+          <div className={bulma.container}>
+            <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block2.html }} />
+          </div>
         </div>
-      </div>
-    </section>
-    <section className={`${bulma.hero} ${bulma['is-medium']} ${bulma['is-bold']} ${bulma['is-info']}`}>
-      <div className={bulma['hero-body']}>
-        <div className={bulma.container}>
-          <div className={bulma.columns}>
-            <div className={bulma.column}>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`}><h1>{data.block3.column1.title}</h1></div>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block3.column1.content.html }} />
-            </div>
-            <div className={bulma.column}>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block3.column2.html }} />
+      </section>
+      <section className={infoHero}>
+        <div className={bulma['hero-body']}>
+          <div className={bulma.container}>
+            <div className={bulma.columns}>
+              <div className={bulma.column}>
+                <div className={mediumCustomContent}><h1>{data.block3.column1.title}</h1></div>
+                <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content} ${styles.columnCluster}`}>
+                  {solutionExamples}
+                </div>
+              </div>
+              <div className={`${bulma.column} ${bulma['is-one-third']}`}>
+                <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block3.column2.html }} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <section className={`${bulma.hero} ${bulma['is-medium']} ${bulma['is-bold']} ${bulma['is-light']}`}>
-      <div className={bulma['hero-body']}>
-        <div className={bulma.container}>
-          <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`}><h1>{data.block4.title}</h1></div>
-          <div className={bulma.columns}>
-            <div className={bulma.column}>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block4.column1.html }} />
-            </div>
-            <div className={bulma.column}>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block4.column2.html }} />
-            </div>
-            <div className={bulma.column}>
-              <div className={`${bulma.content} ${bulma['is-medium']} ${styles.content}`} dangerouslySetInnerHTML={{ __html: data.block4.column3.html }} />
+      </section>
+      <section className={lightHero}>
+        <div className={bulma['hero-body']}>
+          <div className={bulma.container}>
+            <div className={mediumCustomContent}><h1>{data.block4.title}</h1></div>
+            <div className={bulma.columns}>
+              <div className={bulma.column}>
+                <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block4.column1.html }} />
+              </div>
+              <div className={bulma.column}>
+                <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block4.column2.html }} />
+              </div>
+              <div className={bulma.column}>
+                <div className={mediumCustomContent} dangerouslySetInnerHTML={{ __html: data.block4.column3.html }} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+};
 
 SolutionsTemplate.propTypes = {
   data: PropTypes.object.isRequired,
