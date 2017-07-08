2aab7430907e5143746085597f70c71ff711e101
Shawn Stern <stern.shawn@gmail.com>
2017-07-08 14:35:17 -0700

    First pass at navbar with dropdowns + logo
    -All initial links created and styled as a starting point
    
    TODOS:
    -Transparent navbar for easier integration with full screen heros
    -Social links
    -Connect to store for navbar open/close on mobile

--- client/app/components/NavBar/index.js
+++ client/app/components/NavBar/index.js
@@ -3,20 +3,57 @@ import { Link } from 'react-router';
 
 // Styles
 import bulma from 'styles/bulma.scss';
-import styles from './styles.scss';
+import logo from './full_logo.png';
 
-const NavBar = () => (
-  <nav>
-    <Link label="Home" to="/">Home</Link>
-    <Link label="Solutions" to="/solutions">Solutions</Link>
-    <Link label="Verification" to="/verification">Verification</Link>
-    <Link label="Enrichment" to="/enrichment">Enrichment</Link>
-    <Link label="Comparison" to="/comparison">Comparison</Link>
-    <Link label="Telecom" to="/telecom">Telecom</Link>
-    <Link label="Healthcare" to="/healthcare">Healthcare</Link>
-    <Link label="Compliance" to="/compliance">Compliance</Link>
-    <Link label="Blog" to="/blog/page/1">Blog</Link>
-  </nav>
-);
+const NavBar = () => {
+  const hoverableDropdownItem = `${bulma['navbar-item']} ${bulma['has-dropdown']} ${bulma['is-hoverable']}`;
+  const dropdownPrimaryLink = `${bulma['navbar-link']} ${bulma['is-active']}`;
+
+  return (
+    <nav className={bulma.navbar}>
+      <div className={bulma['navbar-brand']}>
+        <Link to={'/'} className={bulma['navbar-item']}>
+          <img src={logo} alt="Idicia" width="112" height="28" />
+        </Link>
+
+        <div className={`${bulma['navbar-burger']} ${bulma.burger}`}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div className={bulma['navbar-menu']}>
+        <div className={bulma['navbar-start']}>
+          <Link to={'/'} className={bulma['navbar-item']}>Home</Link>
+
+          <div className={hoverableDropdownItem}>
+            <Link to={'/solutions'} className={dropdownPrimaryLink}>Solutions</Link>
+            <div className={bulma['navbar-dropdown']}>
+              <Link to={'/verification'} className={bulma['navbar-item']}>Verification</Link>
+              <Link to={'/enrichment'} className={bulma['navbar-item']}>Enrichment</Link>
+              <Link to={'/comparison'} className={bulma['navbar-item']}>Comparison</Link>
+              <Link to={'/telecom'} className={bulma['navbar-item']}>Telecom</Link>
+              <Link to={'/healthcare'} className={bulma['navbar-item']}>Healthcare</Link>
+              <Link to={'/compliance'} className={bulma['navbar-item']}>Compliance</Link>
+            </div>
+          </div>
+          
+          <div className={hoverableDropdownItem}>
+            <Link to={'/resources'} className={dropdownPrimaryLink}>Resources</Link>
+            <div className={bulma['navbar-dropdown']}>
+              <Link to={'/learningcenter'} className={bulma['navbar-item']}>Learning Center</Link>
+            </div>
+          </div>
+
+          <Link to={'/contactus'} className={bulma['navbar-item']}>Contact Us</Link>
+        </div>
+
+        <div className={bulma['navbar-end']}>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
