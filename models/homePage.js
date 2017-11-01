000e94edd7a072f9efd888c4a22e4880fc204e96
Will <will@willacton.com>
2017-11-01 08:54:47 -0700

    fix homePage model

--- models/homePage.js
+++ models/homePage.js
@@ -1,4 +1,4 @@
-const keystone = require('keystone');
+const keystone = require("keystone");
 const Types = keystone.Field.Types;
 
 /**
@@ -7,87 +7,88 @@ const Types = keystone.Field.Types;
  * ==========
  */
 
-const HomePage = new keystone.List('HomePage', {
+const HomePage = new keystone.List("HomePage", {
   map: {
-    name: 'title',
+    name: "title"
   },
   autokey: {
-    path: 'slug',
-    from: 'title',
-    unique: true,
+    path: "slug",
+    from: "title",
+    unique: true
   },
   // nocreate: true,
-  nodelete: true,
+  nodelete: true
 });
 
 HomePage.add({
   title: {
     type: String,
     required: true,
-    default: 'Home',
+    default: "Home"
   },
-  welcome: { 
-    title : {type: Types.Markdown },
+  welcome: {
+    title: { type: Types.Markdown },
     text: { type: Types.Markdown },
-  quickFacts: {
-    title: { type: String },
-    idicia: { 
-      title: { type: Types.Markdown },
-      text: { type: Types.Markdown },
-    },
-    telified: { 
-      title: { type: Types.Markdown },
-      text: { type: Types.Markdown },
-    },
-    digDeeper: { 
-      title: { type: Types.Markdown },
-      text: { type: Types.Markdown },
-    },
-    customers: { 
-      title: { type: Types.Markdown },
-      text: { type: Types.Markdown },
-    },
-    info: { 
-      title: { type: Types.Markdown },
-      text: { type: Types.Markdown },
-    },
-  },
-  whatWeDo: { 
-    text: { type: Types.Markdown },
-    verification: { type: Types.Markdown },
-    enrichment: { type: Types.Markdown },
-    comparison: { type: Types.Markdown },
-  },
-  columns: {
-    contact: {
-      icon: {
-        type: Types.CloudinaryImage,
-        folder: 'idicia/images/pages/homePage/',
-        allowedTypes: 'image/jpeg,image/svg+xml,image/png',
-        autoCleanup : true,
+    quickFacts: {
+      title: { type: String },
+      idicia: {
+        title: { type: Types.Markdown },
+        text: { type: Types.Markdown }
       },
-      text: { type: Types.Markdown },
-    },
-    solutions: {
-      icon: {
-        type: Types.CloudinaryImage,
-        folder: 'idicia/images/pages/homePage/',
-        allowedTypes: 'image/jpeg,image/svg+xml,image/png',
-        autoCleanup : true,
+      telified: {
+        title: { type: Types.Markdown },
+        text: { type: Types.Markdown }
       },
-      text: { type: Types.Markdown },
-    },
-    resources: {
-      icon: {
-        type: Types.CloudinaryImage,
-        folder: 'idicia/images/pages/homePage/',
-        allowedTypes: 'image/jpeg,image/svg+xml,image/png',
-        autoCleanup : true,
+      digDeeper: {
+        title: { type: Types.Markdown },
+        text: { type: Types.Markdown }
       },
-      text: { type: Types.Markdown },
+      customers: {
+        title: { type: Types.Markdown },
+        text: { type: Types.Markdown }
+      },
+      info: {
+        title: { type: Types.Markdown },
+        text: { type: Types.Markdown }
+      }
     },
-  },
+    whatWeDo: {
+      text: { type: Types.Markdown },
+      verification: { type: Types.Markdown },
+      enrichment: { type: Types.Markdown },
+      comparison: { type: Types.Markdown }
+    },
+    columns: {
+      contact: {
+        icon: {
+          type: Types.CloudinaryImage,
+          folder: "idicia/images/pages/homePage/",
+          allowedTypes: "image/jpeg,image/svg+xml,image/png",
+          autoCleanup: true
+        },
+        text: { type: Types.Markdown }
+      },
+      solutions: {
+        icon: {
+          type: Types.CloudinaryImage,
+          folder: "idicia/images/pages/homePage/",
+          allowedTypes: "image/jpeg,image/svg+xml,image/png",
+          autoCleanup: true
+        },
+        text: { type: Types.Markdown }
+      },
+      resources: {
+        icon: {
+          type: Types.CloudinaryImage,
+          folder: "idicia/images/pages/homePage/",
+          allowedTypes: "image/jpeg,image/svg+xml,image/png",
+          autoCleanup: true
+        },
+        text: { type: Types.Markdown }
+      }
+    }
+  }
 });
 
-HomePage.defaultColumns = 'title';
+HomePage.defaultColumns = "title";
 HomePage.register();
