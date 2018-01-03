321378712ac72c22d458b1c5775cc93929feadbb
LeSean Johnson <leseanbiz@gmail.com>
2018-01-02 16:14:43 -0800

    Updates to the footer classes and address

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -13,12 +13,12 @@ const Footer = () => (
   <footer className={`${bulma.hero} ${bulma['is-dark']} ${bulma['is-medium']}`}>
     <div className={`${styles.heroBody}`} >
       <Reveal effect="animated fadeIn">
-        <div className={bulma.container}>          
-          <div className={bulma.columns}>
+      <div className="container">          
+          <div className="columns">
 
             {/* Quicklinks  & Social Media buttons */} 
             {/* These need more vertical space between them */}
-            <div className={`${bulma.column} ${bulma.content} ${bulma['has-text-left']} ${bulma['is-3']} ${bulma['is-offset-1']} ${styles.darkTextFix}`}>
+            <div className={`column content has-text-left is-3 is-offset-1 ${styles.darkTextFix}`}>
                 <h3>Quick Links</h3>
                 <p>
                   <Link to={'/'} className={styles.capsLink}>Home</Link>
@@ -33,7 +33,7 @@ const Footer = () => (
                   <Link to={'/Contact Us'} className={styles.capsLink}>Contact Us</Link>
                 </p>
                 {/* Bring social media icons closer together and add "follow us on:"*/}
-                <hr className={`${styles.hrLeft} ${bulma['has-text-left']}`} />
+                <hr className={`has-text-left ${styles.hrLeft}`} />
                 <div style={{ display: "flex", justifyContent: "center" }}>
                 <div style={{ flex: 1, padding: "5px" }}>
                   <a href="https://www.facebook.com/IDICIA/">
@@ -57,30 +57,30 @@ const Footer = () => (
             </div>
 
               {/* logo and mission statement */}
-            <div className={`${bulma.column} ${bulma.content} ${bulma['has-text-centered']} ${bulma['is-4']} ${styles.darkTextFix}`}>
+            <div className={`column has-text-centered is-4 ${styles.darkTextFix}`}>
                 <img className={styles.footerLogo} src={LargeLogo} alt="Idicia Footer Logo" />
                 <hr/>
                 <p>Integrated Verification, Enrichment and Comparison Solutions for Identity Verification, Fraud Prevention and Compliance.</p>
-                <table className={`${bulma.table} ${bulma['is-narrow']} ${styles.contactTable}`}>
+                <table className={`table is-narrow ${styles.contactTable}`}>
                   {/* How to get these two to sit on the same line?! */}
                   <td className={`${styles.info}`} >
                     <a href="mailto:info@idicia.com">info@idicia.com</a>
                   </td>
                   <td className={`${styles.info}`} >1.844.4IDICIA</td>
                 </table>
-    
             </div>
               
               {/* Contact info */}
-            <div className={`${bulma.column} ${bulma.content} ${bulma['has-text-right']} ${bulma['is-3']} ${styles.darkTextFix}`}>
+            <div className={`column content has-text-right is-3 ${styles.darkTextFix}`}>
               <h3>Locations</h3>
               IDICIA (US)
               4900 SW Griffith Dr, Ste 251
               Beaverton, OR 97005
               <hr className={styles.hrRight} />
               IDICIA (Canada)
-              101A 30701 Simpson Road
-              Abbotsford, BC V2T 6C7
+              273 - 8661 201st Street
+              2nd Floor
+              Langley, BC V2Y 0G9
               <hr className={styles.hrRight} />
             </div>
           </div>
@@ -88,8 +88,8 @@ const Footer = () => (
           
 
           {/* Legal */}
-          <div className={`${bulma.container}`}>
-          <div className={`${bulma.content} ${bulma['has-text-centered']} ${styles.darkTextFix}`}>
+          <div className="container">
+          <div className={`content has-text-centered ${styles.darkTextFix}`}>
                 <p>
                   {`©${year} IDICIA, the Real-time Division of PacificEast. All Rights Reserved.`}
                 </p>
@@ -100,8 +100,8 @@ const Footer = () => (
           </div>
         
         {/* Credits */}
-        <div className={`${bulma.container} ${styles.creditFooter}`}>
-          <div className={`${bulma.content} ${bulma['has-text-centered']} ${styles.darkTextFix}`}>
+        <div className={`container ${styles.creditFooter}`}>
+          <div className={`content has-text-centered ${styles.darkTextFix}`}>
             <p>
               <strong>KeystoneJS + React-Redux + RxJS</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>.
             </p>
