1eef9156cca582274a041dcecca29ea9a193dff5
Shawn Stern <Shawn.Stern@nwea.org>
2018-01-23 12:39:06 -0800

    🎨 Refactoring and removing darkTextFix class, linting, fixing html indentation, removing dead classes/html
    - Adding pading to the top of 'Follow Us' area, the lack of padding looks bad on mobile and doesn't stand out on desktop/tablet view
    - Standardizing padding, etc to use rems over em/px
    - Cleaning up dead classes and refactoring out the darkTextFix class into general footer class, that was a bandaid for improperly using the dark hero long ago, fixed now
    - Generally linting and fixing numerous indentation and spacing syntax errors/linter warnings

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -8,14 +8,11 @@ const year = new Date().getFullYear();
 
 const Footer = () => (
   <footer className={`hero is-dark is-medium ${styles.footerBody}`}>
-  <div className="container is-fluid">
-    <Reveal effect="animated fadeIn">
+    <div className="container is-fluid">
+      <Reveal effect="animated fadeIn">
         <div className="columns">
-          {/* Quicklinks  & Social Media buttons */}
-          {/* These need more vertical space between them */}
-          {/* <div className="column is-1">
-          </div> */}
-          <div className={`column is-3 is-offset-1 has-text-centered-mobile ${styles.darkTextFix}`}>
+          {/* Quicklinks & Social Media buttons */}
+          <div className="column is-3 is-offset-1 has-text-centered-mobile">
             <h3>Quick Links</h3>
             <p>
               <Link to={'/'}>Home</Link>
@@ -29,21 +26,20 @@ const Footer = () => (
             <p>
               <Link to={'/contact-us'}>Contact Us</Link>
             </p>
-            {/* Bring social media icons closer together and add "follow us on:"*/}
-            <hr className={`has-text-left is-hidden-mobile ${styles.hrLeft}`}/>
+            <hr className={`has-text-left is-hidden-mobile ${styles.hrLeft}`} />
             <p>Follow Us On:</p>
-            <div style={{ display: 'flex',}}>
-              <div style={{ flex: 1}}>
+            <div style={{ display: 'flex', paddingTop: '0.75rem' }}>
+              <div style={{ flex: 1 }}>
                 <a href="https://www.facebook.com/IDICIA/">
-                  <i className="fa fa-facebook-official fa-2x" aria-hidden="true"/>
+                  <i className="fa fa-facebook-official fa-2x" aria-hidden="true" />
                 </a>
               </div>
-              <div style={{ flex: 1}}>
+              <div style={{ flex: 1 }}>
                 <a href="https://twitter.com/4idicia?lang=en">
                   <i className="fa fa-twitter-square fa-2x" aria-hidden="true" />
                 </a>
               </div>
-              <div style={{ flex: 1}}>
+              <div style={{ flex: 1 }}>
                 <a href="https://www.linkedin.com/company/idicia">
                   <i className="fa fa-linkedin-square fa-2x" aria-hidden="true" />
                 </a>
@@ -52,7 +48,7 @@ const Footer = () => (
           </div>
 
           {/* logo and mission statement */}
-          <div className={`column content has-text-centered is-4 ${styles.darkTextFix}`}>
+          <div className="column content has-text-centered is-4">
             <img
               className={styles.footerLogo}
               src="images/white.png"
@@ -67,52 +63,50 @@ const Footer = () => (
           </div>
 
           {/* Contact info */}
-          <div className={`column is-3 has-text-right-desktop has-text-right-tablet has-text-centered-mobile ${styles.darkTextFix}`}>
+          <div className="column is-3 has-text-right-desktop has-text-right-tablet has-text-centered-mobile">
             <h3>Locations</h3>
             <span>
               IDICIA (US)
               4900 SW Griffith Dr, Ste 251
               Beaverton, OR 97005
             </span>
-            <hr className={`is-hidden-mobile ${styles.hrRight}`} /><br/>
+            <hr className={`is-hidden-mobile ${styles.hrRight}`} /><br />
             <span>
               IDICIA (Canada)
               273 - 8661 201st Street
               2nd Floor
               Langley, BC V2Y 0G9
             </span>
-            <hr className={`is-hidden-mobile ${styles.hrRight}`}  />
-          </div>
-          <div className="column is-1">
+            <hr className={`is-hidden-mobile ${styles.hrRight}`} />
           </div>
         </div>
 
-      {/* Legal */}
-      <div className="container is-fluid" style={{padding: '5px' }}>
-        <div className={`content has-text-centered ${styles.darkTextFix}`}>
-          <p>
-            {`©${year} IDICIA, the Real-time Division of PacificEast. All Rights Reserved.`}
-          </p>
-          <p>
-            <Link to={'/privacy-policy'}>Privacy Policy</Link>
-          </p>
+        {/* Legal */}
+        <div className="container is-fluid" style={{ padding: '5px' }}>
+          <div className="content has-text-centered">
+            <p>
+              {`©${year} IDICIA, the Real-time Division of PacificEast. All Rights Reserved.`}
+            </p>
+            <p>
+              <Link to={'/privacy-policy'}>Privacy Policy</Link>
+            </p>
+          </div>
         </div>
-      </div>
 
-      {/* Credits */}
-      <div className={`container is-fluid ${styles.creditFooter}`}>
-        <div className={`content has-text-centered ${styles.darkTextFix}`}>
-          <p>
-            <strong>KeystoneJS + React-Redux + RxJS</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>.
-          </p>
-          <p>
-            <a className="icon" href="https://github.com/PurplePigeons/idicia">
-              <i className="fa fa-github fa-3x"></i>
-            </a>
-          </p>
+        {/* Credits */}
+        <div className={`container is-fluid ${styles.creditFooter}`}>
+          <div className="content has-text-centered">
+            <p>
+              <strong>KeystoneJS + React-Redux + RxJS</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>.
+            </p>
+            <p>
+              <a className="icon" href="https://github.com/PurplePigeons/idicia">
+                <i className="fa fa-github fa-3x"></i>
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
-    </Reveal>
+      </Reveal>
     </div>
   </footer>
 );
